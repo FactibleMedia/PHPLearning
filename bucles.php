8f51adfacb6bcf4659bac713dc692ea0b36f0d40
Fez <factiblemedia@gmail.com>
2017-07-22 23:26:21 -0500

    Se aprendió Condicionales y Bucles

---
+++ bucles.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bucles</title>
+</head>
+<body>
+    <?php
+        
+        // $var1=12;
+    
+        /* while($var1<10) { // Bucle indeterminado WHILE
+            echo $var1 . "</br>";
+            $var1++;   
+        }
+        echo "Hemos salido del bucle"; */
+    
+        /* do { // Bucle indeterminado DO WHILE
+            echo $var1 . "</br>";
+            $var1++;
+        } while ($var1<10);
+    
+        echo "Hemos salido del bucle"; */
+        
+        for($i=1;$i<=10;$i++) {
+            
+            //echo " Diego no entiende PHP cuando dice que mi código está mal! "; // Bucle
+        
+            echo "9 x $i = " . 9*$i . "</br>"; // Tabla de multiplicar del 9 
+            
+            
+        }
+    
+        
+
+    
+    ?>
+</body>
+</html>
--- validacion_condicionales.php
+++ validacion_condicionales.php
@@ -4,22 +4,47 @@
         $nombre=$_POST["name"];
         $edad=$_POST["age"];
         
-        if($edad <= 18) {
-            
+        /* if($edad <= 18) {
             echo "Eres menor de edad";
-            
         }else if($edad <= 40){
-            
             echo "Eres jóven";
-            
         }else if($edad <= 65){
-            
             echo "Eres maduro";
         }else{
-            
             echo "Cu&iacutedate";
+        } */
+        
+        /* echo $nombre=="Fez" && $edad=="lalilulelo" ? "Puedes acceder" : "No Puedes acceder"; */  // Línea de trabajo del condiional ternario
+        
+        /* switch (true) { // Cuando es con llaves
             
-        }
+            case $nombre=="Fez" && $edad=="lalilulelo":
+                echo "Usuario Autorizado. Hola Fez";
+                break;
+                
+            default:
+                echo "No puedes entrar, no estás autorizado";
+                // No hace poner el "break;" en éste caso!!
+        } */
+        
+        switch (true) : // Cuando es con dos puntos
+            
+            case $nombre=="Fez" && $edad<=18:
+                echo "Usuario NO Autorizado. Eres menor";
+                break;
+        
+        case $nombre=="Fez" && $edad<=35:
+                echo "Usuario Autorizado. Eres jóven bienvenido";
+                break;
+        
+        case $nombre=="Fez" && $edad<=65:
+                echo "Usuario Autorizado. Eres maduro, ya eres vieja guadia";
+                break;
+                
+            default:
+                echo "No puedes entrar, no estás autorizado";
+                // No hace poner el "break;" en éste caso!!
+        endswitch;
         
     }
 
