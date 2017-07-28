0be0373486b831ee4d6a4b0e6358a7b361123686
Fez <factiblemedia@gmail.com>
2017-07-27 21:46:30 -0500

    Se aprendió funciones predefinidas y también su anidación con otras funciones predefinidas o personales, se aprendió sobre parámetros/argumentos por valor y por referencia, un poco del ya visto Casting y Paso de Parámetros. Se aprendió también sobre parámetros por defecto y ejemplos prácticos.

--- funciones_predefinidas.php
+++ funciones_predefinidas.php
@@ -7,25 +7,70 @@
 <body>
     <?php
     
-        $name = "Fez";
+        $name = "Fez </br></br>";
     
-        echo (strtolower($name));
+        echo (strtolower($name)); // Función anidada (En este caso ambas predefinidas) que convierte los strings a minúsculas o en su caso contrario podemos usar strtoupper
     
         
         function suma($num1, $num2) {
             
-            $num1 = 3;
+            $resultado = $num1 + $num2; // Ejemplo de casting de PHP
             
-            $num2 = "2";
-            
-            $resultado = $num1 + $num2;
-            
-            return $resultado;
-            
+            return $resultado . "</br></br>"; // La función "return" retorna un valor pero no lo devuelve en pantalla
         }
     
-        suma(2,3);
-;    
+        echo suma(2,3); // Ejecutando una función con paso de parametros.
+    
+        
+        function frase_mayus($frase, $conversion=true) { // En el segundo parámetro de la función se declara un parámetro/argumento por defecto, no todos los lenguajes soportan esta función.
+            
+            $frase=strtolower($frase);
+            
+            if($conversion==true) { // En este caso si el segundo parámetro se cumple ejecutará el if
+                
+                $resultado = ucwords($frase); // La función "ucwords" convierte a letra capital.
+                
+            }else{
+                
+                $resultado = strtoupper($frase); // La función mencionada convierte todo a Upercase
+                
+            }
+            
+            return $resultado ."</br></br>"; // Se devuelve el valor
+        }
+    
+        echo frase_mayus("federico"); // Se imprime en pantalla la función con un sólo o con los 2 parámetros (El segundo puede ser false, ya que originalmente era true)
+    
+        
+        function incrementa(&$valor1) { // Se declara la función con un argumento por referencia, en este caso el argumento referenciado crea un vínculo con variables o valores externos fuera de la función y además permite que esos valores sean afectados por lo que se encuentra dentro de la función. Si el parámetro se declara por valor el resultado del programa muestra el 6 y luego el 5
+            
+            $valor1++;
+            
+            return $valor1 . "</br>";
+        }
+    
+        $numero=5;
+    
+        echo incrementa($numero) . "</br>";
+    
+        echo $numero . "</br></br>";
+    
+    
+        function cambiaa_mayus(&$param) {
+            
+            $param = strtolower($param);
+            
+            $param = ucwords($param);
+            
+            return $param;
+        }
+    
+        $cadena = "FEDERICO";
+    
+        echo cambiaa_mayus($cadena) . "</br></br>";
+    
+        echo $cadena . "</br>";
+
     ?>
 </body>
 </html>
