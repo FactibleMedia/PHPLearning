6c879c8a210e4ac514677aba2e8ffd7b21212838
FactibleMedia <factiblemedia@gmail.com>
2020-02-27 10:58:12 -0500

    Actualización
    Update y repaso de conceptos

--- MySQL_deletepage.php
+++ MySQL_deletepage.php
@@ -32,7 +32,7 @@
 
     mysqli_set_charset($db_connect, "utf8"); // Permite usar la codificación UTF8 para mostrar caracteres latinos en el navegador al consultar la BD.
 
-    $db_query = "DELETE FROM productos WHERE codart='$id'"; // Sentencia de consulta a la bd con Mysql
+    $db_query = "DELETE FROM productos WHERE CÓDIGOARTÍCULO='$id'"; // Sentencia de consulta a la bd con Mysql
 
     $db_qresults = mysqli_query($db_connect, $db_query); // result set / record set.
 
@@ -45,12 +45,14 @@
         if (mysqli_affected_rows($db_connect)==0) {
             echo "No se eliminó ningun registro con ese criterio o no existe";
         }else{
-            echo "Se an eliminado " . mysqli_affected_rows($db_connect) . "registros";
+            echo "Se Han eliminado " . mysqli_affected_rows($db_connect) . "registros";
         }
         
     }
 
     mysqli_close($db_connect);  
 
+    // Para actualizar registros en una base de datos utilizamos la siguiente instrucción: UPDATE nombre_tabla SET nombre_campo = valor según el tipo de dato WHERE nombre_campo = valor seguún el tipo de dato. Ejemplo: UPDATE productos SET nombreartículo = 'pantalon', códigoartículo = 'ar41' WHERE paísdeorigen = 'España';
 ?>
+       
         
